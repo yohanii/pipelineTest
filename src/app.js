d12b187e8e0f09ee372e122bc13ca103f32fac2b
yohanii <kim-yohan@gim-yohan-ui-MacBookPro-2.local>
2024-08-02 11:36:55 +0900

    pipeline test1

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, KTB!');
+  res.send('Hello, Yohan!');
 });
 
 app.get('/ping', (req, res) => {
